c85bc408ade775bb2167895655af697c2dfd72b4
Edwin Okoronkwo <49468467+EdwinOkoronkwo@users.noreply.github.com>
2025-03-21 14:25:15 -0600

    added new features (#1)
    * added new features
    
    * added additional features

--- src/main/java/com/edwinokoronkwo/Main.java
+++ src/main/java/com/edwinokoronkwo/Main.java
@@ -3,5 +3,7 @@ package com.edwinokoronkwo;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Added New Features");
+        System.out.println("Added Additional Features");
     }
 }
